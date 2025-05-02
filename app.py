8015ecc0ce9036a2d3ec968f582be3c521723d44
Yannick-Axel <yannick-axel.nanangongang@student.unisg.ch>
2025-05-02 17:47:28 +0200

    new

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.write ("hooray, we connected everything")
 import streamlit as st 
 import pandas as pd
-import seaborn as sns 
+
 import matplotlib.pyplot as plt
 
 import warnings 
